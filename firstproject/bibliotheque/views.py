0ed2de1ec16fe50e97a833a08e6455e4ccc8446a
Ethan68000 <cathycoulon910@gmail.com>
2023-05-15 09:57:26 +0200

    modif

--- firstproject/bibliotheque/views.py
+++ firstproject/bibliotheque/views.py
@@ -18,6 +18,20 @@ def traitement(request):
     lform = LivreForm(request.POST)
     if lform.is_valid():
         livre = lform.save()
-        return render(request,"/bibliotheque/affiche.html",{"livre" : livre})
+        return render(request,"bibliotheque/affiche.html",{"livre" : livre})
     else:
         return render(request,"bibliotheque/ajout.html",{"form": lform})
+
+def affiche(request, id):
+    livre = models.Livre.objects.get(pk=id)
+    return reder(request,"bibliotheque/affiche.html",{"livre": livre})
+
+def traitementupdate(request, id):
+    lform = LivreForm(request.POST)
+    if lform.is_valid():
+        livre = lform.save(commit=False)
+        livre.id = id;
+        livre.save()
+        return HttpResponseRedirect("/bibliotheque/")
+    else:
+        return render(request, "bibliotheque/update.html", {"form": lform, "id": id})
